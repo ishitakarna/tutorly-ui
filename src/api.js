7ce5adf541ec5d0516c743ba42002e66212b0bdd
Ishita Karna <ishitakarna@gmail.com>
2022-12-06 22:07:43 -0600

    api integrated, basic learn added

--- src/api.js
+++ src/api.js
@@ -1,9 +1,27 @@
+import axios from "axios";
+
 export default class Api {
     constructor() {
+        this.client = null;
         this.api_url = "https://llaminators-fp-service.onrender.com/";
     }
 
+    init = () => {
+
+        let headers = {
+            Accept: "application/json",
+        };
+
+        this.client = axios.create({
+            baseURL: this.api_url,
+            timeout: 31000,
+            headers: headers,
+        });
+
+        return this.client;
+    };
+
     getTags = () => {
-        return this.init().get(`${this.api_url}/tags`);
+        return this.init().get(`${this.api_url}tags`);
     };
 }
